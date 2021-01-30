7fef168f289013319365dbf041fcddbef8bf4316
miwanoff <marina.iva.noffa@gmail.com>
2021-01-31 00:22:46 +0200

    add remove button

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import booksData from "./books.js";
+import logo from "./logo.svg";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function Hello() {
+//   return (
+//     <div>
+//       <h1 style={{ color: "red" }}>Hello</h1>
+//       <h2>{booksData[0].name}</h2>
+//     </div>
+//   );
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(<Hello />, document.getElementById("root"));
+function Image(props) {
+  return <img src={props.src} alt="logo" style={{ width: "150px" }} />;
+}
+function Header(props) {
+  return (
+    <div className={props.className}>
+      <Image src={logo} />
+      <h1>Книжный магазин</h1>
+    </div>
+  );
+}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      books: booksData,
+    };
+  }
+  removeBook = (book) => {
+    const updateBooks = this.state.books.filter(function (item) {
+      return item.id != book.id;
+    });
+    console.log(updateBooks);
+    this.setState({
+      books: updateBooks,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Header className="header" />
+        {this.state.books.map((book) => {
+          //console.log(book.id);
+          return (
+            <div key={book.id}>
+              <p>{book.name}</p>
+              <button onClick={this.removeBook.bind(this, book)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
